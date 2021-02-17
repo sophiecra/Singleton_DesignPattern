00d967deabb5cf93b1d92c6cb9a06befb9537042
sophiecra <77420992+sophiecra@users.noreply.github.com>
2021-02-17 13:40:43 -0500

    Edited Library.java

--- Library.java
+++ Library.java
@@ -8,27 +8,25 @@ public class Library {
 
     public static Library getInstance() {
         if (library == null) {
-            System.out.println("Creating a library. Time to begin reading.");
+            System.out.println("Creating our Library. Time to begin reading.");
             library = new Library();
         }
         return library;
     }
 
     public boolean checkoutBook(String bookName) {
-
+        int previousValue = books.value(bookName);
+        if(previousValue > 0) {
+            previousValue -= 1;
+        }
+        books.put(bookName, previousValue);
     }
 
     public void checkInBook(String bookName, int numToAdd) {
-        int previousValue = books.get(numToAdd);
-        if (books.get(bookName) != null) {
-            books.put(bookName, numToAdd);
-        }  else {
-            previousValue = 0;
-            books.put(bookName, numToAdd);
-        }
+        books.put(bookName, numToAdd);
     }
 
     public void displayBooks() {
-
+        int previousValue = books.
     }
 }
