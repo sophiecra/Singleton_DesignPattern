fe54ca82d0683f3bf86eb793cd873c9fb731a364
sophiecra <77420992+sophiecra@users.noreply.github.com>
2021-02-20 10:34:29 -0500

    Added JavaDoc to Library.java

--- Library.java
+++ Library.java
@@ -1,11 +1,21 @@
-
+/**
+ * A library of books
+ * @author Sophie Crane
+ */
 import java.util.HashMap;
 public class Library {
     private HashMap<String, Integer> books = new HashMap<String, Integer>();
     private static Library library;
 
+    /**
+     * Creates an instance of a library
+     */
     private Library() {}
 
+    /**
+     * Creates an instance of a library
+     * @return A library of books
+     */
     public static Library getInstance() {
         if (library == null) {
             System.out.println("Creating our Library. Time to begin reading.");
@@ -14,6 +24,11 @@ public class Library {
         return library;
     }
 
+    /**
+     * Checks out a book from the library
+     * @param bookName Name of the book to be checked out
+     * @return A boolean that indicates if the book is in the library or not
+     */
     public boolean checkoutBook(String bookName) {
         if(books.containsKey(bookName) && books.get(bookName) > 0)
         {
@@ -26,6 +41,11 @@ public class Library {
         }
     }
 
+    /**
+     * Checks in a book to the library
+     * @param bookName Name of the book to be checked out of the library
+     * @param numToAdd The number of books to be added to the library
+     */
     public void checkInBook(String bookName, int numToAdd) {
         if(books.containsKey(bookName)) {
             books.put(bookName, books.get(bookName) + numToAdd);
@@ -35,10 +55,13 @@ public class Library {
         }
     }
 
+    /**
+     * Displays the books in the library to the user
+     */
     public void displayBooks() {
         System.out.println("Inventory: ");
         for (String i : books.keySet()) {
-            System.out.println("- " + i + ", copies: " + books.get(i));
+            System.out.println(" - " + i + ", copies: " + books.get(i));
         }
     }
 }
